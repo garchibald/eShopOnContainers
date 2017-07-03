468950addf9b87953b607880736bab6f3e82b056
Grant Archibald <grarchib@microsoft.com>
2017-07-03 12:37:06 -0700

    Update for windows image testing

--- cli-windows/build-bits.ps1
+++ cli-windows/build-bits.ps1
@@ -1,4 +1,6 @@
-Param([string] $rootPath)
+Param([string] $rootPath,
+[bool] $deleteContainers = $False
+)
 $scriptPath = Split-Path $script:MyInvocation.MyCommand.Path
 
 Write-Host "Current script directory is $scriptPath" -ForegroundColor Yellow
@@ -8,13 +10,27 @@ if ([string]::IsNullOrEmpty($rootPath)) {
 }
 Write-Host "Root path used is $rootPath" -ForegroundColor Yellow
 
+Write-Host "Restoring SNI dll" -ForegroundColor Yellow
+If (Test-Path "nuget.exe") {
+}
+else
+{
+$sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+Invoke-WebRequest $sourceNugetExe -OutFile "nuget.exe"
+}
+.\nuget.exe install runtime.win7-x64.runtime.native.System.Data.SqlClient.sni -Version 4.4.0-beta-25007-02 -Source https://dotnet.myget.org/F/dotnet-core/api/v3/index.json
+$patchSni = $scriptPath + "\runtime.win7-x64.runtime.native.System.Data.SqlClient.sni.4.4.0-beta-25007-02\runtimes\win7-x64\native\sni.dll"
+
 $projectPaths = 
     @{Path="$rootPath\src\Web\WebMVC";Prj="WebMVC.csproj"},
     @{Path="$rootPath\src\Web\WebSPA";Prj="WebSPA.csproj"},
     @{Path="$rootPath\src\Services\Identity\Identity.API";Prj="Identity.API.csproj"},
     @{Path="$rootPath\src\Services\Catalog\Catalog.API";Prj="Catalog.API.csproj"},
     @{Path="$rootPath\src\Services\Ordering\Ordering.API";Prj="Ordering.API.csproj"},
-    @{Path="$rootPath\src\Services\Basket\Basket.API";Prj="Basket.API.csproj"}
+    @{Path="$rootPath\src\Services\Basket\Basket.API";Prj="Basket.API.csproj"},
+    @{Path="$rootPath\src\Services\Locations\Locations.API";Prj="Locations.API.csproj"},
+    @{Path="$rootPath\src\Services\Marketing\Marketing.API";Prj="Marketing.API.csproj"},
+    @{Path="$rootPath\src\Services\GracePeriod\GracePeriod.API";Prj="GracePeriod.API.csproj"},
     @{Path="$rootPath\src\Web\WebStatus";Prj="WebStatus.csproj"}
 
 $projectPaths | foreach {
@@ -22,12 +38,17 @@ $projectPaths | foreach {
     $projectFile = $_.Prj
     $outPath = $_.Path + "\obj\Docker\publish"
     $projectPathAndFile = "$projectPath\$projectFile"
+    $sniFile = $_.Path + "\obj\Docker\publish\runtimes\win7-x64\native\sni.dll"
     Write-Host "Deleting old publish files in $outPath" -ForegroundColor Yellow
     remove-item -path $outPath -Force -Recurse -ErrorAction SilentlyContinue
     Write-Host "Publishing $projectPathAndFile to $outPath" -ForegroundColor Yellow
     dotnet restore $projectPathAndFile
     dotnet build $projectPathAndFile
     dotnet publish $projectPathAndFile -o $outPath
+    if ( Test-path $sniFile ) {
+        Write-Host "Applying Patch SNI dll  $sniFile" -ForegroundColor Yellow
+        Copy-Item -path $patchSni -Destination $sniFile -Force
+    }
 }
 
 
@@ -35,19 +56,22 @@ $projectPaths | foreach {
 # Delete old eShop Docker images
 ########################################################################################
 
-$imagesToDelete = docker images --filter=reference="eshop/*" -q
-
-If (-Not $imagesToDelete) {Write-Host "Not deleting eShop images as there are no eShop images in the current local Docker repo."} 
-Else 
+if ( $deleteContainers -eq $true )
 {
-    # Delete all containers
-    Write-Host "Deleting all containers in local Docker Host"
-    docker rm $(docker ps -a -q) -f
+    $imagesToDelete = docker images --filter=reference="eshop/*" -q
+
+    If (-Not $imagesToDelete) {Write-Host "Not deleting eShop images as there are no eShop images in the current local Docker repo."} 
+    Else 
+    {
+        # Delete all containers
+        Write-Host "Deleting all containers in local Docker Host"
+        docker rm $(docker ps -a -q) -f
     
-    # Delete all eshop images
-    Write-Host "Deleting eShop images in local Docker repo"
-    Write-Host $imagesToDelete
-    docker rmi $(docker images --filter=reference="eshop/*" -q) -f
+        # Delete all eshop images
+        Write-Host "Deleting eShop images in local Docker repo"
+        Write-Host $imagesToDelete
+        docker rmi $(docker images --filter=reference="eshop/*" -q) -f
+    }
 }
 
 # WE DON'T NEED DOCKER BUILD AS WE CAN RUN "DOCKER-COMPOSE BUILD" OR "DOCKER-COMPOSE UP" AND IT WILL BUILD ALL THE IMAGES IN THE .YML FOR US
--- cli-windows/start-windows-containers.ps1
+++ cli-windows/start-windows-containers.ps1
@@ -1,9 +1,83 @@
-Param([string] $rootPath)
+Param([string] $rootPath,
+[bool] $azure = $True,
+[bool] $images = $True,
+[bool] $build = $false)
 $scriptPath = Split-Path $script:MyInvocation.MyCommand.Path
 if ([string]::IsNullOrEmpty($rootPath)) {
     $rootPath = "$scriptPath\.."
 }
 Write-Host "Root path used is $rootPath" -ForegroundColor Yellow
 
-& .\build-bits.ps1 -rootPath $rootPath
-docker-compose -f "$rootPath\docker-compose-windows.yml" -f "$rootPath\docker-compose-windows.override.yml"  up
+if ( $build )
+{
+	& .\build-bits.ps1 -rootPath $rootPath
+}
+
+$composeFile = $rootPath + "\docker-compose-windows.yml"
+$overrideFile = $rootPath + "\docker-compose-windows.override.yml"
+
+if ( $azure ) {
+	$errors = $false
+	if ( Test-Path env:ESHOP_REDIS_CONNECTION ) {
+		Write-Host "Using configured ESHOP_REDIS_CONNECTION"
+	} else {
+		Write-Host "Missing environment variable ESHOP_REDIS_CONNECTION" -ForegroundColor Yellow
+		$errors = $True
+	}
+	
+	if ( Test-Path env:ESHOP_EVENT_BUS_CONNECTION ) {
+		Write-Host "Using configured ESHOP_EVENT_BUS_CONNECTION"
+	} else {
+		Write-Host "Missing environment variable ESHOP_EVENT_BUS_CONNECTION" -ForegroundColor Yellow
+		$errors = $True
+	}
+	
+	if ( Test-Path env:ESHOP_AZUREDB_CONNECTION ) {
+		Write-Host "using configured ESHOP_AZUREDB_CONNECTION"
+	} else {
+		Write-Host "Missing environment variable ESHOP_AZUREDB_CONNECTION" -ForegroundColor Yellow
+		$errors = $True
+	}
+	
+	if ( Test-Path env:AZURE_COSMOSDB ) {
+		Write-Host "using configured AZURE_COSMOSDB"
+	} else {
+		Write-Host "Missing environment variable AZURE_COSMOSDB" -ForegroundColor Yellow
+		$errors = $True
+	}
+	
+	if ( Test-Path env:ESHOP_REPOSITORY ) {
+		Write-Host "Using configured ESHOP_REPOSITORY " + env:ESHOP_REPOSITORY
+	} else {
+		Write-Host "NOTE: Missing environment variable ESHOP_REPOSITORY" -ForegroundColor Yellow
+	}
+	
+	if ( Test-Path env:ESHOP_AZURE_DB_PREFIX ) {
+		Write-Host "Using configured ESHOP_AZURE_DB_PREFIX $env:ESHOP_AZURE_DB_PREFIX"
+	} else {
+		Write-Host "NOTE: Missing environment variable ESHOP_AZURE_DB_PREFIX" -ForegroundColor Yellow
+	}
+
+	if ( $errors ) {
+		return
+	}
+
+	$overrideFile = $rootPath + "\docker-compose-windows.azure.yml"
+}
+
+if ( $images ) {
+	$composeFile = $rootPath + "\docker-compose-windows-image.yml"
+}
+
+if ( Test-Path env:TAG ) {
+	# Use defined value
+} else {
+	$env:TAG=dev
+}
+
+Write-Host "Using Tag $env:TAG"
+pushd ..
+
+docker-compose -f $composeFile -f $overrideFile  up --force-recreate
+
+popd
