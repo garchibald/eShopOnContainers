76c46fa1c2d49a406145f9865c3a63b717aa510f
Grant Archibald <grarchib@microsoft.com>
2017-07-09 17:57:13 -0700

    Update to docker management script

--- cli-windows/docker-images.ps1
+++ cli-windows/docker-images.ps1
@@ -3,34 +3,98 @@
     [string] $sourceTag = 'dev',
     [string] $destinationTag = 'latest',
     [bool] $tag = $true,
-    [bool] $push = $false
+    [string] $operation = "push",
+	[string] $resources = "eshop"
 )
+#-----------------------------------------------------------------
+# Docker Image Management script
+#
+# Common scenarios
+#
+# 1) Pull common windows container images
+#
+# .\docker-images.ps1 $null $null $null $false "pull" "common"
+#
+# 2) Shared container images
+#
+# .\docker-images.ps1 "your-repository.azurecr.io $null $null $false "pull" "shared"
+#
+# 3) eshop container images
+#
+# .\docker-images.ps1 "your-repository.azurecr.io "nanowin" $null $false "pull" "eshop"
+#
+#-----------------------------------------------------------------
 
 if ( $repository.Length -gt 0 ) {
     $repository = $repository + "/"
 }
 
-$images = 
-      @{ name = "eshop/catalog.api" },
-      @{ name = "eshop/ordering.api" },
-      @{ name = "eshop/marketing.api" },
-      @{ name = "eshop/webspa" },
-	  @{ name = "eshop/webmvc" },
-      @{ name = "eshop/basket.api" },
-      @{ name = "eshop/identity.api" },
-      @{ name = "eshop/graceperiodmanager" },
-      @{ name = "eshop/payment.api" },
-      @{ name = "eshop/locations.api" },
-      @{ name = "eshop/webstatus" }
+switch ( $resources )
+{
+	 "eshop" {
+		$images = 
+		  @{ name = "eshop/catalog.api" },
+		  @{ name = "eshop/ordering.api" },
+		  @{ name = "eshop/marketing.api" },
+		  @{ name = "eshop/webspa" },
+		  @{ name = "eshop/webmvc" },
+		  @{ name = "eshop/basket.api" },
+		  @{ name = "eshop/identity.api" },
+		  @{ name = "eshop/graceperiodmanager" },
+		  @{ name = "eshop/payment.api" },
+		  @{ name = "eshop/locations.api" },
+		  @{ name = "eshop/webstatus" }
+	}
+	"shared" {
+		$images =
+		  @{ name = "mongodb" },
+		  @{ name = "redis" },
+		  @{ name = "rabbitmq" }
+	}
+	"common" {
+		$images =
+		  @{ name = "microsoft/windowsservercore" },
+		  @{ name = "microsoft/nanoserver" },
+		  @{ name = "microsoft/mssql-server-windows" }
+	}
+}
+
+
+if ( $resources -eq "pull" ) {
+	$tag = $false
+}
+
+Write-Host "Images:"
+
+$images
 
 $images  | foreach {
-    $source = $_.name + ":" + $sourceTag
-    $target = $repository + $_.name + ":" +$destinationTag
+    $source = $_.name
+	if ( $sourceTag.Length -gt 0 ) {
+		$source = $source + ":" + $sourceTag
+	}
+	
+    $target = $repository + $_.name
+	if ( $destinationTag.Length -gt 0 ) {
+		$target = $target + ":" +$destinationTag
+	}
+	
     if ( $tag ) {
+        Write-Host "Tag $source $target"
         docker tag $source $target
     }
 
-    if ( $push ) {
-        docker push $target
-    }
+	switch ( $operation )
+	{
+		"push" {
+            Write-Host "Push $target"
+			docker push $target
+		}
+		
+		"pull" {
+            $source = $repository + $source
+            Write-Host "Pull $source"
+			docker pull $source
+		}
+	}
 }
